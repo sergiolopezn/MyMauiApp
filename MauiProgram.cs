fac89ce87fde81c6dbccd8d5b7e539703f41ed51
sergiolopez <sergio.lopez@nttdata.com>
2026-03-18 12:17:26 -0600

    Connectivity validation

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace MyMauiApp;
 
@@ -13,7 +14,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			}).UseMauiCommunityToolkit();
+		
+		builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
 		builder.Services.AddSingleton<MainPage>();
 		builder.Services.AddSingleton<ViewModel.MainViewModel>();
 		
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Alerts;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -6,9 +7,24 @@ namespace MyMauiApp.ViewModel;
 
 public partial class MainViewModel : ObservableObject
 {
+    private IConnectivity connectivity;
     [ObservableProperty] string newTask = string.Empty;
 
     [ObservableProperty] ObservableCollection<string> tasks = new();
+    
+    public MainViewModel(IConnectivity connectivity)
+    {
+        this.connectivity = connectivity;
+        checkConnection();
+    }
+
+    private void checkConnection()
+    {
+        if (connectivity.NetworkAccess != NetworkAccess.Internet)
+        {
+            Toast.Make("No internet connection").Show();
+        }
+    }
 
     [RelayCommand]
     public void AddTask(string task)
